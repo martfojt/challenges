5cb50929b3f2ed404529ff50affbb1fbf7119b4f
Martin Fojtek <martfojt@gmail.com>
2024-01-21 14:56:24 +0100

    Add javascript functionality and a new project

--- faq-accordion-main/javascript.js
+++ faq-accordion-main/javascript.js
@@ -1,10 +1,33 @@
+// function toggleParagraph(paragraphId) {
+//   const paragraph = document.getElementById(paragraphId);
+//   const showParagraph = paragraph.style.display;
+
+//   if (showParagraph === 'block') {
+//     paragraph.style.display = 'none';
+//   } else {
+//     paragraph.style.display = 'block';
+//   }
+// }
+
 function toggleParagraph(paragraphId) {
   const paragraph = document.getElementById(paragraphId);
-  const showParagraph = paragraph.style.display;
+  const header = document.querySelector(
+    `h3[onclick="toggleParagraph('${paragraphId}')"]`,
+  );
 
-  if (showParagraph === 'block') {
-    paragraph.style.display = 'none';
-  } else {
-    paragraph.style.display = 'block';
-  }
+  // Toggle paragraph display
+  paragraph.style.display =
+    paragraph.style.display === 'block' ? 'none' : 'block';
+
+  // Toggle the icon image source
+  const icon = header.querySelector('.icon');
+  icon.src =
+    paragraph.style.display === 'block'
+      ? './assets/images/icon-minus.svg'
+      : './assets/images/icon-plus.svg';
+}
+
+function areAnyParagraphsVisible() {
+  const paragraphs = document.querySelectorAll('.main-faq-text p');
+  return Array.from(paragraphs).some((para) => para.style.display === 'block');
 }
